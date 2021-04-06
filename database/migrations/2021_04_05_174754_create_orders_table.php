d4208abb29815e7f749c18acf0406166ae7827e6
danielamoudokpo <62663366+danielamoudokpo@users.noreply.github.com>
2021-04-06 23:03:00 +0200

    Fresh database

---
+++ database/migrations/2021_04_05_174754_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('client_id');
+            $table->integer('pharmacy_id');
+            $table->string('address');
+            $table->string('has_prescription')->nullable();
+            $table->string('status')->nullable();
+            $table->string('comments')->nullable();
+            $table->string('price')->nullable();
+            $table->dateTime('order_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
