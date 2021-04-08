fb07b5c22aa1a6bb1a3b068a4fc2497b6b7e7896
lailaess <laila.essadouqiepitech.eu>
2021-04-09 01:10:55 +0200

    implentation de login/register

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('register', [UserController::class, 'register']); 
+    Route::post('login', [UserController::class, 'login']); 
+    // Route::post('logout', [UserController::class, 'logout']); 
+
 });
+
+Route::post('register', [UserController::class, 'register']); 
+Route::post('login', [UserController::class, 'login']); 
+// Route::post('logout', [UserController::class, 'logout']); 
