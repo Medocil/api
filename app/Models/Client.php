5c226c4969f1ef2f1d5c7b3fedf5dd3f168ebd4e
lailaess <62251801+lailaess@users.noreply.github.com>
2021-04-11 16:22:00 +0200

    implementations des models (#10)
    Co-authored-by: lailaess <laila.essadouqiepitech.eu>

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    public $timestamps = true;
+    protected $fillable = [
+        'social_security_number',
+    
+    ];
+    
+    public function order()
+    {
+        return $this->hasMany(Order::class); 
+    }
+    
+    public function prescription()
+    {
+        return $this->hasMany(Prescription::class);
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
